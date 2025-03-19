3a0d7a56df0b0a7fff1052c2ef4fce551e23abe3
kirgudu1 <sitezabota@gmail.com>
2025-03-19 13:31:04 +0300

    Add files via upload

---
+++ переименование пхп файлов.py
@@ -0,0 +1,21 @@
+import os
+
+# Получаем текущую директорию, в которой находится скрипт
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Перебираем все файлы в текущей директории
+for filename in os.listdir(current_directory):
+    # Проверяем, содержит ли имя файла нужный фрагмент
+    if ".php.html" in filename:
+        # Создаём новое имя, заменяя фрагмент
+        new_filename = filename.replace(".php.html", ".html")
+        
+        # Формируем полные пути
+        old_path = os.path.join(current_directory, filename)
+        new_path = os.path.join(current_directory, new_filename)
+        
+        # Переименовываем файл
+        os.rename(old_path, new_path)
+        print(f'Renamed: {filename} -> {new_filename}')
+
+print("Все файлы переименованы!")
